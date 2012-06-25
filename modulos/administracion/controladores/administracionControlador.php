791b6d1144723ed3ba72646c3150fa2d72086f23
Ernesto Rubio F <neto.r27@gmail.com>
2012-06-25 01:28:09 -0500

    Administración de usuarios
    Página de administración de usuarios

--- modulos/administracion/controladores/administracionControlador.php
+++ modulos/administracion/controladores/administracionControlador.php
@@ -4,6 +4,7 @@ function principal() {
     if (validarUsuarioAdministrador()) {
         require_once 'modulos/administracion/vistas/principal.php';
     } else {
+        setSessionMessage("no eres administrador");
         goToIndex();
     }
 }
