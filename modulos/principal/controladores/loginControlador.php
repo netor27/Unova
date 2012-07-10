d4bb1991c1be49a14c0893801a304e09f33e80b4
Ernesto Rubio F <neto.r27@gmail.com>
2012-07-09 23:37:04 -0500

    Facebook Login
    Permite hacer login desde cualquier página y es reenviado a esa misma página

--- modulos/principal/controladores/loginControlador.php
+++ modulos/principal/controladores/loginControlador.php
@@ -5,47 +5,7 @@ function principal() {
     require_once 'modulos/principal/modelos/loginModelo.php';
     require_once 'modulos/usuarios/modelos/usuarioModelo.php';
 
-    if ($user) {
-        //tenemos un usuario logeado en facebook
-        //Datos de facebook
-        $email = $user_info['email'];
-        $nombre = $user_info['name'];
-        $avatar = 'http://graph.facebook.com/' . $user . '/picture?type=normal';
-
-        //validamos si este usuario ya tiene su email registrado, sino creamos un usuario nuevo
-        $usuario = getUsuarioFromEmail($email);
-        if (isset($usuario)) {
-            //el usuario ya existe en la bd, loggearlo!
-            if(loginUsuario($usuario->email, $usuario->password) == 1)
-                setSessionMessage("<h4 class='success'>¡Bienvenido " . getUsuarioActual()->nombreUsuario . "!</h4>");
-            goToIndex();
-        } else {
-            //el usuario no existe en la bd, crearlo!
-            require_once 'modulos/usuarios/clases/Usuario.php';
-            require_once 'funcionesPHP/uniqueUrlGenerator.php';
-            $usuario = new Usuario();
-            $usuario->nombreUsuario = $nombre;
-            $usuario->email = $email;
-            $password = getUniqueCode(10);
-            $usuario->password = md5($password);
-            $usuario->uniqueUrl = getUsuarioUniqueUrl($nombre);
-
-            $array = altaUsuario($usuario);
-            $id = $array['id'];
-            $usuario->idUsuario = $id;
-
-            if ($id >= 0) {
-                //Ya creamos el usuario, ahora actualizamos su avatar y su estado de activado
-                echo 'se creo un usuario con id= ' . $id;
-                $usuario->avatar = $avatar;
-                actualizaAvatar($usuario);
-                setActivado($id, 1);
-                if(loginUsuario($email, md5($password)) == 1)
-                    setSessionMessage("<h4 class='success'>¡Bienvenido " . getUsuarioActual()->nombreUsuario . "!</h4>");
-                goToIndex();
-            }
-        }
-    } else {
+    if (!$user) {
         $pagina = "/";
         require_once 'modulos/principal/vistas/login.php';
     }
