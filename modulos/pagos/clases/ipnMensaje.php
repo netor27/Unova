2c1d8ec573bf0cbdd532145c892b173b13e2c9bf
Ernesto Rubio F <neto.r27@gmail.com>
2012-08-03 01:47:30 -0500

    Modulo Pagos

--- modulos/pagos/clases/ipnMensaje.php
+++ modulos/pagos/clases/ipnMensaje.php
@@ -26,23 +26,23 @@ class IpnMensaje {
 
     function toString() {
         $res = "Mensaje recibido " + date("d/m/Y  H:i:s");
-        $res.=" <br> txn_type => " . $txn_type;
-        $res.=" <br> txn_id => " . $txn_id;
-        $res.=" <br> receirver_email =>" . $receiver_email;
-        $res.=" <br> item_name =>" . $item_name;
-        $res.=" <br> item_number =>" . $item_number;
-        $res.=" <br> payment_status =>" . $payment_status;
-        $res.=" <br> parent_txt_id =>" . $parent_txt_id;
-        $res.=" <br> mc_gross (cantidad depositada)=>" . $mc_gross;
-        $res.=" <br> mc_fee (comision) =>" . $mc_fee;
-        $res.=" <br> mc_currency =>" . $mc_currency;
-        $res.=" <br> first_name =>" . $first_name;
-        $res.=" <br> last_name =>" . $last_name;
-        $res.=" <br> payer_email =>" . $payer_email;        
-        $res.=" <br> payment_date =>" . $payment_date;
-        $res.=" <br> test_ipn =>" . $test_ipn;
-        $res.=" <br> custom =>" . $custom;
-        $res.=" <br><br><br><br> Complete post<br><br><br> " . $complete_post;
+        $res.=" <br> txn_type => " . $this->txn_type;
+        $res.=" <br> txn_id => " . $this->txn_id;
+        $res.=" <br> receirver_email =>" . $this->receiver_email;
+        $res.=" <br> item_name =>" . $this->item_name;
+        $res.=" <br> item_number =>" . $this->item_number;
+        $res.=" <br> payment_status =>" . $this->payment_status;
+        $res.=" <br> parent_txt_id =>" . $this->parent_txt_id;
+        $res.=" <br> mc_gross (cantidad depositada)=>" . $this->mc_gross;
+        $res.=" <br> mc_fee (comision) =>" . $this->mc_fee;
+        $res.=" <br> mc_currency =>" . $this->mc_currency;
+        $res.=" <br> first_name =>" . $this->first_name;
+        $res.=" <br> last_name =>" . $this->last_name;
+        $res.=" <br> payer_email =>" . $this->payer_email;        
+        $res.=" <br> payment_date =>" . $this->payment_date;
+        $res.=" <br> test_ipn =>" . $this->test_ipn;
+        $res.=" <br> custom =>" . $this->custom;
+        $res.=" <br><br><br><br> Complete post<br><br><br> " . $this->complete_post;
         return $res;
     }
 
