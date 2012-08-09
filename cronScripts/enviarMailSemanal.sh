9cab072460dfdf3580fd13eef0047e2e2213225a
Ernesto Rubio F <neto.r27@gmail.com>
2012-08-08 20:06:04 -0500

    Cron scripts
    Crons actualizados para mostrar la fecha

--- cronScripts/enviarMailSemanal.sh
+++ cronScripts/enviarMailSemanal.sh
@@ -1,6 +1,12 @@
 #
+# Cron, agregar cron -e, borrar cron -r, listar cron -l
+#
+#Se manda llamar cada domingo a las 5 am con este cron:
+#   00 5 * * 0 /home/neto/public_html/unova/cronScripts/enviarMailSemanal.sh
+#
 #Script que manda llamar http://unova.co/email.php?llaveSecreta=199201302
 #
-#wget -O - -q http://unova.co/email.php?llaveSecreta=199201302
-echo "Se envio mail semanal" >> /home/neto/logs/mailSemanal.log
+wget -O - -q http://unova.co/email.php?llaveSecreta=199201302
+date=`date`
+echo Se ejecuto script mail semanal $date >> /home/neto/logs/mailSemanal.log
 exit 0
--- cronScripts/tranformarVideo.sh
+++ cronScripts/tranformarVideo.sh
@@ -1,6 +1,12 @@
 #
+#Se manda llamar cada minuto con el comando de cron:
+#  * * * * *  /home/neto/public_html/unova/cronScripts/tranformarVideo.sh
+#Se manda llamar cada 2 minutos con el comando de cron:
+#  */2 * * * *  /home/neto/public_html/unova/cronScripts/tranformarVideo.sh
+#
 #Script que manda llamar 
 #
 #wget -O - -q http://unova.co/email.php?llaveSecreta=199201302
-echo "Se transformara el video" >> /home/neto/logs/transformarVideo.log
+date=`date`
+echo Transformacion de video $date >> /home/neto/logs/transformarVideo.log
 exit 0
