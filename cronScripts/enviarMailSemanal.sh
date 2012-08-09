c52262255e773eddba30a9012617f111e54deafd
Ernesto Rubio F <neto.r27@gmail.com>
2012-08-08 19:42:16 -0500

    Add CronScripts
    Se agregaron scipts que se ejecutaran con cron

---
+++ cronScripts/enviarMailSemanal.sh
@@ -0,0 +1,6 @@
+#
+#Script que manda llamar http://unova.co/email.php?llaveSecreta=199201302
+#
+#wget -O - -q http://unova.co/email.php?llaveSecreta=199201302
+echo "Se envio mail semanal" >> /home/neto/logs/mailSemanal.log
+exit 0
---
+++ cronScripts/tranformarVideo.sh
@@ -0,0 +1,6 @@
+#
+#Script que manda llamar 
+#
+#wget -O - -q http://unova.co/email.php?llaveSecreta=199201302
+echo "Se transformara el video" >> /home/neto/logs/transformarVideo.log
+exit 0
