85f3604915a5010768ef37d8dede9bf6599f60d7
Ernesto Rubio F <neto.r27@gmail.com>
2012-08-20 11:41:08 -0500

    Enviar Mail Semanal
    Fixed enviarMailSemanal.sh

--- modulos/email/controladores/emailControlador.php
+++ modulos/email/controladores/emailControlador.php
@@ -27,31 +27,31 @@ function enviarResumenSemanal() {
         echo "Total de usuarios = " . $totalUsuarios . "\n\n";
         //obtenemos de 500 en 500 usuarios
         $i = 0;
-        $n = 0;
         $numEnviados = 0;
+        $numConfirmados = 0;
         for ($i = 0; $i <= $totalUsuarios; $i+=500) {
             $usuarios = getUsuariosParaResumenSemanal($i, 500);
+            $numConfirmados += sizeof($usuarios);
             foreach ($usuarios as $usuario) {
-                echo "\n================================== \n  ";
-                echo 'Analizando usuario ' . $usuario->idUsuario . ' -- ' . $usuario->nombreUsuario . " \n  ";
+                //echo "\n================================== \n  ";
+                //echo 'Analizando usuario ' . $usuario->idUsuario . ' -- ' . $usuario->nombreUsuario . " \n  ";
                 //Obtener el número de nuevos alumnos en sus cursos
                 $numAlumnos = getNumeroDeNuevosAlumnos($usuario->idUsuario, $dias);
-                echo '---Alumnos nuevos ' . $numAlumnos . " \n  ";
+                //echo '---Alumnos nuevos ' . $numAlumnos . " \n  ";
                 //Obtener el número de preguntas sin responder
                 $numPreguntas = getNumeroDePreguntasSinResponder($usuario->idUsuario);
-                echo '---Preguntas sin responder ' . $numPreguntas . " \n  ";
+                //echo '---Preguntas sin responder ' . $numPreguntas . " \n  ";
                 require_once 'modulos/email/modelos/envioEmailModelo.php';
                 if ($numAlumnos > 0 || $numPreguntas > 0) {
                     //enviarMailResumenSemanal($usuario->email, $usuario->nombreUsuario, $numAlumnos, $numPreguntas);
-                    echo 'mail enviado a ' . $usuario->email ." \n  ";
+                    //echo 'mail enviado a ' . $usuario->email ." \n  ";
                     $numEnviados++;
-                }else{
-                    echo "no se envio el mail porque tiene 0 alumnos y 0 preguntas \n  ";
+                } else {
+                    //echo "no se envio el mail porque tiene 0 alumnos y 0 preguntas \n  ";
                 }
-                $n++;
             }
         }
-        echo "Se envio el mail a " . $numEnviados . " usuarios de " . $n . " usuarios con cuenta confirmada <br>\n";
+        echo "Usuarios: Total=" . $totalUsuarios . ";confirmados=;" . $numConfirmados . "mailsEnviados=" . $numEnviados;
     }
 //    else {
 //        goToIndex();
